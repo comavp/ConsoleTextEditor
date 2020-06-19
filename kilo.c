816e8908f9752299d34eec9d85ac7facec976605
Anton <comavp@yandex.ru>
2020-06-19 11:10:14 +0400

    Add colorful single-line comments
    Specify single-line comment pattern for the C filetype.

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@ enum editorKey {
 
 enum editorHighligh {
 	HL_NORMAL = 0,
+	HL_COMMENT,
 	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
@@ -52,6 +53,7 @@ enum editorHighligh {
 struct editorSyntax {
 	char* filetype;
 	char** filematch;
+	char* singleline_comment_start;
 	int flags;
 };
 
@@ -87,6 +89,7 @@ struct editorSyntax HLDB[] = {
 	{
 		"c",
 		C_HL_extensions,
+		"//",
 		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
 };
@@ -224,6 +227,9 @@ void editorUpdateSyntax(erow* row) {
 	
 	if (E.syntax == NULL) return;
 	
+	char* scs = E.syntax->singleline_comment_start;
+	int scs_len = scs ? strlen(scs) : 0;
+	
 	int prev_sep = 1;
 	int in_string = 0;
 	
@@ -231,6 +237,12 @@ void editorUpdateSyntax(erow* row) {
 	while (i < row->rsize) {
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+		
+		if (scs_len && !in_string) {
+			if (!strncmp(&row->render[i], scs, scs_len)) {
+				memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+			}
+		}
 		
 		if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
 			if (in_string) {
@@ -271,6 +283,7 @@ void editorUpdateSyntax(erow* row) {
 
 int editorSyntaxToColor(int hl) {
 	switch (hl) {
+		case HL_COMMENT: return 36;
 		case HL_STRING: return 35;
 		case HL_NUMBER: return 31;
 		case HL_MATCH: return 34;
